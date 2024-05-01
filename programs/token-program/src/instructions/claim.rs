72ed67f42bbbe1d308ba94dba9008c588ab75f01
tarunjais28 <tarunjais28@gmail.com>
2024-05-01 12:48:04 +0530

    Added claim

---
+++ programs/token-program/src/instructions/claim.rs
@@ -0,0 +1,74 @@
+use super::*;
+
+/// Function to transfer the tokens
+///
+/// This function can throw following errors:
+///   - Amount Can't Be Zero (when user passes 0 amount for mint).
+pub fn claim_royalty(ctx: Context<ClaimTokens>, token: String) -> Result<()> {
+    let caller = ctx.accounts.authority.to_account_info().key();
+
+    let cpi_program = ctx.accounts.token_program.to_account_info();
+
+    let seeds = &[MINT_TAG, token.as_bytes(), &[ctx.bumps.mint_account]];
+    let signer = [&seeds[..]];
+
+    // Create the Transfer struct for our context
+    let cpi_accounts = TransferChecked {
+        mint: ctx.accounts.mint_account.to_account_info(),
+        to: ctx.accounts.to_account.to_account_info(),
+        authority: ctx.accounts.authority.to_account_info(),
+        from: ctx.accounts.escrow_account.to_account_info(),
+    };
+
+    token_2022::transfer_checked(
+        CpiContext::new_with_signer(cpi_program, cpi_accounts, &signer),
+        ctx.accounts.escrow_account.amount,
+        ctx.accounts.mint_account.decimals,
+    )?;
+
+    // Emit transfer event
+    emit!(TransferEvent {
+        token: token,
+        amount: ctx.accounts.escrow_account.amount,
+        from: caller,
+        to: ctx.accounts.to_account.to_account_info().key()
+    });
+
+    Ok(())
+}
+
+#[derive(Accounts)]
+#[instruction(token: String)]
+pub struct ClaimTokens<'info> {
+    #[account(
+        mut,
+        seeds = [MAINTAINERS_TAG],
+        bump,
+    )]
+    pub maintainers: Account<'info, Maintainers>,
+
+    /// CHECK: This is the token that we want to mint
+    #[account(
+        mut,
+        seeds = [MINT_TAG, token.as_bytes()],
+        bump,
+    )]
+    pub mint_account: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        mut,
+        seeds = [ESCROW_TAG, token.as_bytes()],
+        bump,
+    )]
+    pub escrow_account: InterfaceAccount<'info, TokenAccount>,
+
+    /// CHECK: This is the token account that we want to transfer tokens to
+    #[account(mut)]
+    pub to_account: AccountInfo<'info>,
+
+    /// CHECK: the authority of the mint account
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    pub token_program: Program<'info, Token2022>,
+}
