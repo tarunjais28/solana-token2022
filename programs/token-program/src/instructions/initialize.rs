e95a81cd68a1d5fcd93dec18a19c3034ab9a5f95
tarunjais28 <tarunjais28@gmail.com>
2024-05-01 12:48:36 +0530

    Added Initialization

---
+++ programs/token-program/src/instructions/initialize.rs
@@ -0,0 +1,49 @@
+use super::*;
+
+/// Function to initialize the contract
+pub fn initialize(ctx: Context<Initialize>, whitelisted_users: Vec<Pubkey>) -> Result<()> {
+    let caller = ctx.accounts.authority.to_account_info().key();
+    let maintainers = &mut ctx.accounts.maintainers;
+    maintainers.save(caller);
+
+    let whitelist = &mut ctx.accounts.whitelist;
+    whitelist.save(whitelisted_users);
+
+    // Emit init event
+    emit!(InitEvent {
+        admin: caller,
+        sub_admin: caller
+    });
+
+    Ok(())
+}
+
+#[derive(Accounts)]
+#[instruction(whitelisted_users: Vec<Pubkey>)]
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        seeds = [MAINTAINERS_TAG],
+        bump,
+        payer = authority,
+        space = std::mem::size_of::<Maintainers>() + 32
+    )]
+    pub maintainers: Account<'info, Maintainers>,
+
+    /// CHECK: Whitelist
+    #[account(
+        init,
+        seeds = [WHITELIST_TAG],
+        bump,
+        payer = authority,
+        space = std::mem::size_of::<WhitelistedUser>() + (whitelisted_users.len() * 32),
+    )]
+    pub whitelist: Account<'info, WhitelistedUser>,
+
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+
+    pub token_program: Program<'info, Token2022>,
+}
