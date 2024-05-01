872a742a3a3ecdc21c792be44bcfad3db32a68b9
tarunjais28 <tarunjais28@gmail.com>
2024-05-01 12:46:57 +0530

    Constants

---
+++ programs/token-program/src/constants.rs
@@ -0,0 +1,16 @@
+use super::*;
+
+#[constant]
+pub const CONFIG_TAG: &[u8] = b"config";
+
+#[constant]
+pub const MINT_TAG: &[u8] = b"mint";
+
+#[constant]
+pub const MAINTAINERS_TAG: &[u8] = b"maintainers";
+
+#[constant]
+pub const WHITELIST_TAG: &[u8] = b"whitelist";
+
+#[constant]
+pub const ESCROW_TAG: &[u8] = b"escrow";
