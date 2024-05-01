269d986eecde03dd48035d676342f1d4d623143e
tarunjais28 <tarunjais28@gmail.com>
2024-05-01 12:47:38 +0530

    Added burn from

---
+++ programs/token-program/src/instructions/burn_from.rs
@@ -0,0 +1,73 @@
+use super::*;
+
+/// Function to burn the tokens fom
+///
+/// This function can throw following errors:
+///   - Amount Can't Be Zero (when user passes 0 amount for burn).
+pub fn burn_from(ctx: Context<BurnTokenFrom>, params: TokenParams) -> Result<()> {
+    let sub_admins = &ctx.accounts.maintainers.sub_admins;
+    let caller = ctx.accounts.authority.to_account_info().key();
+
+    // Ensuring authorized sender
+    require!(sub_admins.contains(&caller), CustomError::Unauthorized);
+
+    // Check amount first
+    require!(params.amount > 0, CustomError::AmountCantBeZero);
+
+    let seeds = &[MINT_TAG, params.name.as_bytes(), &[ctx.bumps.mint_account]];
+    let signer = [&seeds[..]];
+
+    let cpi_program = ctx.accounts.token_program.to_account_info();
+
+    // Create the MintTo struct for our context
+    let cpi_accounts = Burn {
+        mint: ctx.accounts.mint_account.to_account_info(),
+        from: ctx.accounts.from.to_account_info(),
+        authority: ctx.accounts.mint_account.to_account_info(),
+    };
+    token_2022::burn(
+        CpiContext::new_with_signer(cpi_program, cpi_accounts, &signer),
+        params.amount,
+    )?;
+
+    // Emit burn event
+    emit!(BurnEvent {
+        token: params.name,
+        amount: params.amount
+    });
+
+    Ok(())
+}
+
+#[derive(Accounts)]
+#[instruction(params: TokenParams)]
+pub struct BurnTokenFrom<'info> {
+    #[account(
+        seeds = [MAINTAINERS_TAG],
+        bump,
+    )]
+    pub maintainers: Account<'info, Maintainers>,
+
+    /// CHECK: This is the token that we want to mint
+    #[account(
+        mut,
+        seeds = [MINT_TAG, params.name.as_bytes()],
+        bump,
+    )]
+    pub mint_account: InterfaceAccount<'info, Mint>,
+
+    /// CHECK: This is the token account that we want to burn tokens from
+    #[account(mut)]
+    pub from: AccountInfo<'info>,
+
+    /// CHECK: the authority of the mint account
+    #[account(mut)]
+    pub authority: Signer<'info>,
+
+    #[account(mut)]
+    pub token_account: InterfaceAccount<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token2022>,
+
+    pub system_program: Program<'info, System>,
+}
---
+++ programs/token-program/src/lib.rs
@@ -0,0 +1,99 @@
+use crate::{constants::*, enums::*, errors::*, events::*, instructions::*, states::*, structs::*};
+use anchor_lang::{prelude::*, solana_program::program::invoke};
+use anchor_spl::{
+    token_2022::{self, Burn, MintTo, Token2022, TransferChecked},
+    token_interface::{Mint, TokenAccount, TokenInterface},
+};
+use spl_token_2022::{extension::ExtensionType, state::Mint as MintState};
+pub use structs::TokenParams;
+
+mod constants;
+mod enums;
+mod errors;
+mod events;
+mod instructions;
+mod states;
+mod structs;
+
+declare_id!("8akcs6EFZN1KQCkVNQoswS1UpaTs2mxKEfF2L4nui57A");
+
+#[program]
+pub mod token_program {
+    use super::*;
+
+    pub fn init(ctx: Context<Initialize>, whitelisted_users: Vec<Pubkey>) -> Result<()> {
+        instructions::initialize(ctx, whitelisted_users)
+    }
+
+    pub fn manage_admin(ctx: Context<UpdateAdmin>, address: Pubkey) -> Result<()> {
+        instructions::update_admin(ctx, address)
+    }
+
+    pub fn add_sub_admin_accounts(
+        ctx: Context<UpdateSubAdmins>,
+        addresses: Vec<Pubkey>,
+    ) -> Result<()> {
+        instructions::add_sub_admins(ctx, addresses)
+    }
+
+    pub fn remove_sub_admin_accounts(
+        ctx: Context<UpdateSubAdmins>,
+        addresses: Vec<Pubkey>,
+    ) -> Result<()> {
+        instructions::remove_sub_admins(ctx, addresses)
+    }
+
+    pub fn create(ctx: Context<CreateToken>, params: CreateTokenParams) -> Result<()> {
+        instructions::create_token(ctx, params)
+    }
+
+    pub fn mint_token(ctx: Context<MintToken>, params: TokenParams) -> Result<()> {
+        instructions::mint(ctx, params)
+    }
+
+    pub fn burn_token(ctx: Context<BurnToken>, params: TokenParams) -> Result<()> {
+        instructions::burn(ctx, params)
+    }
+
+    pub fn burn_token_from(ctx: Context<BurnTokenFrom>, params: TokenParams) -> Result<()> {
+        instructions::burn_from(ctx, params)
+    }
+
+    pub fn manange_whitelist_users(
+        ctx: Context<WhitelistUser>,
+        update_type: UpdateType,
+        users: Vec<Pubkey>,
+    ) -> Result<()> {
+        instructions::manange_whitelist(ctx, update_type, users)
+    }
+
+    pub fn transfer_tokens(ctx: Context<TransferTokens>, params: TransferParams) -> Result<()> {
+        instructions::transfer(ctx, params)
+    }
+
+    pub fn force_transfer_tokens(
+        ctx: Context<ForceTransferTokens>,
+        params: ForceTransferParams,
+    ) -> Result<()> {
+        instructions::force_transfer(ctx, params)
+    }
+
+    pub fn claim(ctx: Context<ClaimTokens>, token: String) -> Result<()> {
+        instructions::claim_royalty(ctx, token)
+    }
+
+    pub fn update_royalty(
+        ctx: Context<UpdateTokenConfig>,
+        token: String,
+        royalty: u8,
+    ) -> Result<()> {
+        instructions::update_royalty_percentage(ctx, token, royalty)
+    }
+
+    pub fn buy_with_sol(
+        ctx: Context<BuyWithSol>,
+        params: BuyWithSolParams
+    ) -> Result<()> {
+        instructions::buy_token_with_sol(ctx, params)
+    }
+}
