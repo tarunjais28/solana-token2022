f0dc7af3c9f3cb1feb9c89af68ff7318eb4bac31
tarunjais28 <tarunjais28@gmail.com>
2024-05-01 12:50:46 +0530

    Added types

---
+++ target/types/ico.ts
@@ -0,0 +1,537 @@
+export type Ico = {
+  "version": "0.1.0",
+  "name": "ico",
+  "instructions": [
+    {
+      "name": "createIcoAta",
+      "accounts": [
+        {
+          "name": "icoAtaForIcoProgram",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "data",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "icoMint",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "icoAtaForAdmin",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "admin",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "tokenProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "rent",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "icoAmount",
+          "type": "u64"
+        },
+        {
+          "name": "solPrice",
+          "type": "u64"
+        },
+        {
+          "name": "usdtPrice",
+          "type": "u64"
+        }
+      ]
+    },
+    {
+      "name": "depositIcoInAta",
+      "accounts": [
+        {
+          "name": "icoAtaForIcoProgram",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "data",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "icoMint",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "icoAtaForAdmin",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "admin",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "tokenProgram",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "icoAmount",
+          "type": "u64"
+        }
+      ]
+    },
+    {
+      "name": "buyWithSol",
+      "accounts": [
+        {
+          "name": "icoAtaForIcoProgram",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "data",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "icoMint",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "icoAtaForUser",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "user",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "admin",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "tokenProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "icoAtaForIcoProgramBump",
+          "type": "u8"
+        },
+        {
+          "name": "solAmount",
+          "type": "u64"
+        }
+      ]
+    },
+    {
+      "name": "buyWithUsdt",
+      "accounts": [
+        {
+          "name": "icoAtaForIcoProgram",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "data",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "icoMint",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "icoAtaForUser",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "usdtAtaForUser",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "usdtAtaForAdmin",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "user",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "tokenProgram",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "icoAtaForIcoProgramBump",
+          "type": "u8"
+        },
+        {
+          "name": "usdtAmount",
+          "type": "u64"
+        }
+      ]
+    },
+    {
+      "name": "updateData",
+      "accounts": [
+        {
+          "name": "data",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "admin",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "solPrice",
+          "type": "u64"
+        },
+        {
+          "name": "usdtPrice",
+          "type": "u64"
+        }
+      ]
+    }
+  ],
+  "accounts": [
+    {
+      "name": "data",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "sol",
+            "type": "u64"
+          },
+          {
+            "name": "usdt",
+            "type": "u64"
+          },
+          {
+            "name": "admin",
+            "type": "publicKey"
+          }
+        ]
+      }
+    }
+  ]
+};
+
+export const IDL: Ico = {
+  "version": "0.1.0",
+  "name": "ico",
+  "instructions": [
+    {
+      "name": "createIcoAta",
+      "accounts": [
+        {
+          "name": "icoAtaForIcoProgram",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "data",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "icoMint",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "icoAtaForAdmin",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "admin",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "tokenProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "rent",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "icoAmount",
+          "type": "u64"
+        },
+        {
+          "name": "solPrice",
+          "type": "u64"
+        },
+        {
+          "name": "usdtPrice",
+          "type": "u64"
+        }
+      ]
+    },
+    {
+      "name": "depositIcoInAta",
+      "accounts": [
+        {
+          "name": "icoAtaForIcoProgram",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "data",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "icoMint",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "icoAtaForAdmin",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "admin",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "tokenProgram",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "icoAmount",
+          "type": "u64"
+        }
+      ]
+    },
+    {
+      "name": "buyWithSol",
+      "accounts": [
+        {
+          "name": "icoAtaForIcoProgram",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "data",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "icoMint",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "icoAtaForUser",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "user",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "admin",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "tokenProgram",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "icoAtaForIcoProgramBump",
+          "type": "u8"
+        },
+        {
+          "name": "solAmount",
+          "type": "u64"
+        }
+      ]
+    },
+    {
+      "name": "buyWithUsdt",
+      "accounts": [
+        {
+          "name": "icoAtaForIcoProgram",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "data",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "icoMint",
+          "isMut": false,
+          "isSigner": false
+        },
+        {
+          "name": "icoAtaForUser",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "usdtAtaForUser",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "usdtAtaForAdmin",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "user",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "tokenProgram",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "icoAtaForIcoProgramBump",
+          "type": "u8"
+        },
+        {
+          "name": "usdtAmount",
+          "type": "u64"
+        }
+      ]
+    },
+    {
+      "name": "updateData",
+      "accounts": [
+        {
+          "name": "data",
+          "isMut": true,
+          "isSigner": false
+        },
+        {
+          "name": "admin",
+          "isMut": true,
+          "isSigner": true
+        },
+        {
+          "name": "systemProgram",
+          "isMut": false,
+          "isSigner": false
+        }
+      ],
+      "args": [
+        {
+          "name": "solPrice",
+          "type": "u64"
+        },
+        {
+          "name": "usdtPrice",
+          "type": "u64"
+        }
+      ]
+    }
+  ],
+  "accounts": [
+    {
+      "name": "data",
+      "type": {
+        "kind": "struct",
+        "fields": [
+          {
+            "name": "sol",
+            "type": "u64"
+          },
+          {
+            "name": "usdt",
+            "type": "u64"
+          },
+          {
+            "name": "admin",
+            "type": "publicKey"
+          }
+        ]
+      }
+    }
+  ]
+};
