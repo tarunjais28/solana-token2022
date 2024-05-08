fbcb9b6c26283319e697a7af7347e71fda3d52c0
tarunjais28 <tarunjais28@gmail.com>
2024-05-08 10:00:18 +0530

    Added constants

---
+++ programs/receiver/src/constants.rs
@@ -0,0 +1,10 @@
+use super::*;
+
+#[constant]
+pub const MAINTAINERS_TAG: &[u8] = b"maintainers";
+
+#[constant]
+pub const USER_DATA_TAG: &[u8] = b"user_data";
+
+#[constant]
+pub const ESCROW_TAG: &[u8] = b"escrow";
